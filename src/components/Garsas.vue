85f9cb92a264de1977d5b8b5e4025c586324bb4a
svajunas svajunas <svajunas@aventusgroup.com>
2018-12-17 14:39:56 +0200

    more data

--- src/components/Garsas.vue
+++ src/components/Garsas.vue
@@ -33,6 +33,7 @@
         props: ["garsas"],
         mounted() {
             let data = this.$store.getters.getAllData;
+            this.title = data.garsas_page.garsas_title.name;
             this.description = data.garsas_page.garsas;
         }
     }
