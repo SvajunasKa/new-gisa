e37b957403c735e0f883891007bb66f8425e6749
svajunas svajunas <svajunas@aventusgroup.com>
2018-12-17 17:38:02 +0200

    garas done

--- src/components/Garsas.vue
+++ src/components/Garsas.vue
@@ -1,82 +1,118 @@
 <template>
-    <div class="content">
-        <div class="title">
-            <h1>{{title}}</h1>
-        </div>
-        <div class="container">
-            <section v-for="(garsas, index) in description">
-                <div class="row"  v-if="index%2 === 0">
-                    <div class="col-md-4">
-                        <img :src="garsas.img">
-                    </div>
-                    <div class="col-md-8">
-                        <p v-html="garsas.description"></p>
-                        <p v-html="garsas.kiekis"></p>
-                        <div class="text" v-if="garsas.more">
-                            <a @click="showModal(garsas.info)" v-html="garsas.more"></a>
-                        </div>
-                    </div>
-                </div>
-                <div class="row" v-else>
-                    <div class="col-md-8">
-                        <p v-html="garsas.description"></p>
-                        <p v-html="garsas.kiekis"></p>
-                        <div class="text" v-if="garsas.more">
-                            <a @click="showModal(garsas.info)" v-html="garsas.more"></a>
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <img :src="garsas.img">
-                    </div>
-                </div>
-                <hr>
-            </section>
-        </div>
-        <modal v-if="isModalVisible" @close="closeModal" :garsas="parametrai"></modal>
+  <div class="content">
+    <div class="title">
+      <h1>{{title}}</h1>
     </div>
+    <div class="container">
+      <section v-for="(garsas, index) in description">
+        <div class="row" v-if="index%2 === 0">
+          <div class="col-md-4">
+            <div class="img-box">
+              <img :src="garsas.img" @click="showModal(garsas)">
+            </div>
+
+          </div>
+          <div class="col-md-8">
+            <div class="text-block">
+              <p v-html="garsas.description"></p>
+              <p v-html="garsas.kiekis"></p>
+              <div class="text" v-if="garsas.more">
+                <a @click="showModal(garsas.info)" v-html="garsas.more"></a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row" v-else>
+          <div class="col-md-8">
+            <div class="text-block">
+              <p v-html="garsas.description"></p>
+              <p v-html="garsas.kiekis"></p>
+              <div class="text" v-if="garsas.more">
+                <a @click="showModal(garsas.info)" v-html="garsas.more"></a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="img-box">
+              <img :src="garsas.img" @click="showModal(garsas)">
+            </div>
+
+          </div>
+        </div>
+        <hr>
+      </section>
+    </div>
+    <modal v-if="isModalVisible" @close="closeModal" :garsas="parametrai"></modal>
+  </div>
 
 </template>
 
 <script>
-    import modal from './elements/Popup-new'
+  import modal from './elements/Popup-new'
 
-    export default {
-        name: "garsas",
-        data() {
-            return {
-                title: null,
-                description: null,
-                isModalVisible: false,
-                parametrai: "",
-            }
-        },
-        props: ["garsas"],
-        mounted() {
-            let data = this.$store.getters.getAllData;
-            this.title = data.garsas_page.garsas_title.name;
-            this.description = data.garsas_page.garsas;
-        },
-        methods: {
-            showModal(parametrai) {
-                this.isModalVisible = true;
-                this.parametrai = parametrai;
-                console.log(this.parametrai)
-            },
-            closeModal() {
-                this.isModalVisible = false;
-            }
-        },
-        components: {
-            modal: modal
-        }
+  export default {
+    name: "garsas",
+    data() {
+      return {
+        title: null,
+        description: null,
+        isModalVisible: false,
+        parametrai: "",
+      }
+    },
+    props: ["garsas"],
+    mounted() {
+      let data = this.$store.getters.getAllData;
+      this.title = data.garsas_page.garsas_title.name;
+      this.description = data.garsas_page.garsas;
+    },
+    methods: {
+      showModal(parametrai) {
+        this.isModalVisible = true;
+        this.parametrai = parametrai;
+      },
+      closeModal() {
+        this.isModalVisible = false;
+      }
+    },
+    components: {
+      modal: modal
     }
+  }
 </script>
 
 <style lang="scss" scoped>
-    .title {
-        @include title();
+  .title {
+    background-image: -webkit-linear-gradient(top, rgba(0, 0, 0, .5), rgba(0, 0, 0, .5)), url("../assets/images/audio1.jpg");
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+    @include title();
+  }
+
+  section {
+    //height: 250px;
+  }
+
+  .img-box {
+    width: 200px;
+    height: 300px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    img {
+      width: 100%;
+      cursor: pointer;
     }
-    img{
-        width: 100%;
-    }
+  }
+
+  .text-block {
+    text-align: center;
+    display: flex;
+    align-items: center;
+    height: 300px;
+    flex-flow: column;
+    justify-content: center;
+  }
+
 </style>
--- src/components/elements/Popup-new.vue
+++ src/components/elements/Popup-new.vue
@@ -1,138 +1,162 @@
 <template>
-    <transition name="modal-fade">
-        <div class="modal-backdrop ">
-            <div class="modal">
-                <div v-if="komplektai">
-                    <div class="button-container">
-                        <button type="button"
-                                class="btn-close"
-                                @click="close">×
-                        </button>
-                    </div>
-                    <div class="modal-body ">
-                        <div v-for="(key, value) in papildomaInfo">
-                            <p><b v-html="value"></b></p>
-                            <p v-for="data in key" v-html="data"></p>
-                        </div>
-                    </div>
-                </div>
-                <div  v-if="sviesos">
-                    <div>
-                        <button type="button"
-                                class="btn-close"
-                                @click="close">×
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                       <iframe :src="sviesos"></iframe>
-                    </div>
-                </div>
-                <div v-if="garsas">
-                    <button type="button"
-                            class="btn-close"
-                            @click="close">×
-                    </button>
-                    <div class="modal-body ">
-                        <div v-for="xxx in garsas">
-                           <p><b>{{xxx.title}}</b></p>
-                            <p v-for="aaa in xxx.aprasymas" v-if="xxx.aprasymas" v-html="aaa"></p>
-                        </div>
-                    </div>
-                </div>
+  <transition name="modal-fade">
+    <div class="modal-backdrop ">
+      <div class="container">
+        <div class="modal">
+          <div v-if="komplektai">
+            <div class="button-container">
+              <button type="button"
+                      class="btn-close"
+                      @click="close">×
+              </button>
             </div>
+            <div class="modal-body ">
+              <div v-for="(key, value) in papildomaInfo">
+                <p><b v-html="value"></b></p>
+                <p v-for="data in key" v-html="data"></p>
+              </div>
+            </div>
+          </div>
+          <div v-if="sviesos">
+            <div>
+              <button type="button"
+                      class="btn-close"
+                      @click="close">×
+              </button>
+            </div>
+            <div class="modal-body">
+              <iframe :src="sviesos"></iframe>
+            </div>
+          </div>
+          <div v-if="garsas">
+            <div class="button-container">
+              <button type="button"
+                      class="btn-close"
+                      @click="close">×
+              </button>
+            </div>
+            <div class="modal-body ">
+              <img v-if="garsas.img" :src="garsas.img">
+              <div v-else  v-for="item in garsas">
+                <div  class="modal-title">
+                  <h3><b>{{item.title}}</b></h3>
+                </div>
+                <div class="modal-content">
+                  <p v-for="aaa in item.aprasymas" v-if="item.aprasymas" v-html="aaa"></p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    </transition>
+      </div>
+
+    </div>
+  </transition>
 </template>
 
 <script>
 
-    export default {
-        name: "modal",
-        props: ['komplektai', "sviesos", "garsas"],
-        methods: {
-            close() {
-                this.$emit('close');
-            }
-        },
-    }
+  export default {
+    name: "modal",
+    props: ['komplektai', "sviesos", "garsas"],
+    methods: {
+      close() {
+        this.$emit('close');
+      }
+    },
+  }
 </script>
 
 <style scoped lang="scss">
-    .modal-backdrop {
-        position: fixed;
-        top: 0;
-        bottom: 0;
-        left: 0;
-        right: 0;
-        background-color: rgba(0, 0, 0, 0.7);
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        z-index: 9999;
-    }
+  .modal-backdrop {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background-color: rgba(0, 0, 0, 0.7);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 9999;
+  }
 
+  .modal {
+    background: #FFFFFF;
+    border-radius: 5px;
+    box-shadow: 2px 2px 20px 1px;
+    display: flex;
+    flex-direction: column;
+    max-width: calc(100% - 200px);
+    height: auto;
+    max-height: 500px;
+    margin: 0 auto;
+    iframe {
+      width: 560px;
+      height: 315px;
+      border: none;
+    }
+  }
+
+  .modal-fade-enter,
+  .modal-fade-leave-active {
+    opacity: 0;
+  }
+
+  .modal-fade-enter-active,
+  .modal-fade-leave-active {
+    transition: opacity 0.8s ease
+  }
+
+  .modal-body {
+    position: relative;
+    padding: 0 40px 50px 40px;
+    color: black;
+    overflow-y: auto;
+    height: auto;
+    max-height: 450px;
+    h3, p {
+      text-align: center;
+    }
+    .modal-content {
+      //height: 350px;
+      display: flex;
+     justify-content: center;
+      flex-flow: column;
+    }
+    img{
+      width: 100%;
+    }
+  }
+
+  .button-container {
+    position: relative;
+    height: 50px;
+    .btn-close {
+      right: -10px;
+      top: -25px;
+      position: absolute;
+      border: none;
+      padding: 20px;
+      cursor: pointer;
+      font-weight: 700;
+      color: #aaa;
+      font-family: $font;
+      font-size: 50px;
+      background: transparent;
+    }
+  }
+
+  .btn-green {
+    color: white;
+    background: #4AAE9B;
+    border: 1px solid #4AAE9B;
+    border-radius: 2px;
+  }
+
+  @media screen and (max-width: $break-tablet) {
     .modal {
-        background: #FFFFFF;
-        border-radius: 5px;
-        box-shadow: 2px 2px 20px 1px;
-        display: flex;
-        flex-direction: column;
-        max-width: calc(100% - 200px);
-        height: auto;
-        max-height: 500px;
-        iframe{
-            width: 560px ;
-            height: 315px;
-            border: none;
-        }
+      max-width: calc(100% - 5px);
     }
-
-    .modal-fade-enter,
-    .modal-fade-leave-active {
-        opacity: 0;
-    }
-
-    .modal-fade-enter-active,
-    .modal-fade-leave-active {
-        transition: opacity 0.8s ease
-    }
-
-    .modal-body {
-        position: relative;
-        padding: 20px 40px;
-        color: black;
-        overflow-y: auto;
-        height: 450px;
-    }
-    .button-container{
-        position: relative;
-        height: 50px;
-        .btn-close {
-            right: -10px;
-            top: -25px;
-            position: absolute;
-            border: none;
-            padding: 20px;
-            cursor: pointer;
-            font-weight: 700;
-            color: #aaa;
-            font-family: $font;
-            font-size: 50px;
-            background: transparent;
-        }
-    }
-
-
-
-    .btn-green {
-        color: white;
-        background: #4AAE9B;
-        border: 1px solid #4AAE9B;
-        border-radius: 2px;
-    }
-    @media screen and (max-width: $break-tablet){
-        .modal{
-            max-width: calc(100% - 5px);
-        }
-    }
+  }
 </style>
